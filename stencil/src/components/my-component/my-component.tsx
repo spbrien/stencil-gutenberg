45659537d020dc03fb25691974291d324090fb30
Steven Brien <spbrien@gmail.com>
2018-12-20 08:43:03 -0600

    Turn off shadow dowm for SEO

--- stencil/src/components/my-component/my-component.tsx
+++ stencil/src/components/my-component/my-component.tsx
@@ -3,7 +3,7 @@ import { Component, Prop } from '@stencil/core';
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true
+  shadow: false
 })
 export class MyComponent {
   /**
